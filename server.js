f3662e808b8be79b26af5343324e158b1f77739a
mhmdhalawi <mhmdhalawi@users.noreply.github.com>
2020-03-07 22:06:32 +0200

    adjustments

--- server.js
+++ server.js
@@ -36,6 +36,6 @@ app.get('/profile/:id', (req, res) => profile.handleProfile(req, res, database))
 app.put('/image', (req, res) => image.handleImage(req, res, database));
 app.post('/imageurl', (req, res) => image.handleApiCall(req, res));
 
-app.listen(3000, () => {
-  console.log('app is running on port 3000');
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`app is running on port ${process.env.PORT}`);
 });
